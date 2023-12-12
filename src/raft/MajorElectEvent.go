9a675e7c1dcd840088e1ce733bae39ea9273af4f
root <root@localhost.localdomain>
2023-12-12 16:34:13 +0800

    update: update sendSingleHL

--- src/raft/MajorElectEvent.go
+++ src/raft/MajorElectEvent.go
@@ -6,17 +6,24 @@ type SendHLTimeout struct {
 	machine *RaftStateMachine
 }
 
+func (rf *Raft) makeMajorElected() *SendHLTimeout {
+	return &SendHLTimeout{
+		machine: rf.stateMachine,
+	}
+}
+
 func (trans *SendHLTimeout) isRWMu() bool {
 	return false
 }
 
 //
 // init volatile state when peer firstTime electing
+// this make sure raft can only submit logEntry in its own term
 //
-func (rf *Raft) initVolatileState() {
-	for i := 0; i < rf.PeerCount(); i++ {
-		rf.stateMachine.nextIndex[i] = rf.stateMachine.lastLogIndex() + 1
-		rf.stateMachine.matchIndex[i] = 0
+func (machine *RaftStateMachine) initVolatileState(peerCount int) {
+	for i := 0; i < peerCount; i++ {
+		machine.nextIndex[i] = machine.lastLogIndex() + 1
+		machine.matchIndex[i] = 0
 	}
 }
 
@@ -30,11 +37,16 @@ func (trans *SendHLTimeout) transfer(source SMState) SMState {
 	// the first time to send HA, init volatileState
 	if source == startElectionState {
 		trans.machine.raft.print("first sendHL after elect, start init volatileState")
-		trans.machine.raft.initVolatileState()
+		trans.machine.raft.stateMachine.initVolatileState(trans.machine.raft.PeerCount())
 	}
 	trans.machine.raft.electionTimer.Stop()
+
+	if trans.machine.raft.killed() {
+		return noTransferState
+	}
 	go trans.machine.raft.sendHLs()
 	trans.machine.raft.sendHETimer.Start()
+	trans.machine.raft.persist()
 	return startSendHEState
 }
 
@@ -59,9 +71,10 @@ func (rf *Raft) sendHLs() {
 	cond := sync.NewCond(&sync.Mutex{})
 	joincount := 0
 	for i := 0; i < rf.PeerCount(); i++ {
-		if i != rf.me {
-			go rf.sendSingleHL(i, &joincount, cond)
+		if i == rf.me {
+			continue
 		}
+		go rf.sendSingleHL(i, &joincount, cond)
 	}
 	cond.L.Lock()
 	for joincount+1 < rf.PeerCount() {
@@ -74,19 +87,31 @@ func (rf *Raft) sendHLs() {
 // send single peer heartBeat/appendEntries Message
 //
 func (rf *Raft) sendSingleHL(server int, join *int, cond *sync.Cond) {
+	sendRpc := false
+	rf.stateMachine.rwmu.RLock()
+	// if peer's state change, don't send the left appendEntriesRPC
 	if rf.stateMachine.CurState == startSendHEState {
+		sendRpc = true
+	}
+	rf.stateMachine.rwmu.RUnlock()
+	if sendRpc {
+		rf.stateMachine.rwmu.RLock()
 		args := AppendEntriesArgs{
 			Term:     rf.stateMachine.currentTerm,
 			LeaderId: rf.me,
 		}
 		rf.initHLArgsLog(server, &args)
+		rf.stateMachine.rwmu.RUnlock()
 		reply := AppendEntriesReply{}
+		rf.print("leader %d send AppendEntriesRPC to %d", rf.me, server)
 		ok := rf.sendAppendEntires(server, &args, &reply)
+		rf.stateMachine.rwmu.RLock()
 		if ok {
-			rf.print("receive heartBeatRpc reply from %d success %t", server, reply.Success)
+			rf.print("receive AppendEntriesRPC reply from %d success %t", server, reply.Success)
+
 			// case curState change
 			if reply.Term > rf.stateMachine.currentTerm {
-				rf.stateMachine.issueTrans(&LargerTerm{machine: rf.stateMachine, newTerm: reply.Term})
+				rf.stateMachine.issueTrans(rf.makeLargerTermEvent(reply.Term, args.LeaderId))
 			} else {
 				// resolve AppendEntriesReply
 				rf.stateMachine.issueTrans(rf.makeResolveHLReply(&reply, &args, server))
@@ -94,6 +119,7 @@ func (rf *Raft) sendSingleHL(server int, join *int, cond *sync.Cond) {
 		} else {
 			rf.stateMachine.raft.print("unreachable appendEntries to %d", server)
 		}
+		rf.stateMachine.rwmu.RUnlock()
 	}
 	cond.L.Lock()
 	*join++
